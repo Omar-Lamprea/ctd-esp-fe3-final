1b953f367a1bf8ee290c841e9e5b9f28713cdfc9
Omar-Lamprea <lampreaomar@gmail.com>
2023-04-07 12:17:48 -0500

    css files

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import './Styles/DarkTheme.css'
+import './Styles/QueryMobile.css'
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import ContextProvider from './Components/utils/global.context';
